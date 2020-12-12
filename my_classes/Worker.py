0bf83f6195911108e683fd63d27c3ce232d4c0e4
Steven Phun <phun.steven@gmail.com>
2020-12-12 12:04:08 -0800

    fixed is_circulating()

--- my_classes/Worker.py
+++ my_classes/Worker.py
@@ -7,12 +7,12 @@ class Worker:
         self.course = course  # the course object the tutor is tutoring.
         self.reaction_msg = None  # the reaction message sent by the tutor to get the next student.
 
-    async def is_circulating(self):
+    def is_circulating(self):
         """checks if the tutor's reaction message is still awaiting a response.
 
         :return: True if the tutor's reaction message is still circulating the queue, otherwise return False.
         """
-        return self.reaction_msg is None
+        return self.reaction_msg is not None
 
     def hours(self):
         """convert the tutor's current session hours to a string.
