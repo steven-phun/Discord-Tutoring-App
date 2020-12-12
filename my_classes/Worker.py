460ad64e3225f408bdf1a2a2bd3bb99b71084a13
Steven Phun <phun.steven@gmail.com>
2020-12-11 17:16:14 -0800

    added is_circulating()

--- my_classes/Worker.py
+++ my_classes/Worker.py
@@ -2,6 +2,14 @@ class Worker:
     def __init__(self, discord_id, course):
         self.discord_id = discord_id  # the tutor's discord id.
         self.course = course  # the course object the tutor is tutoring.
+        self.reaction_msg = None  # the reaction message sent by the tutor to get the next student.
+
+    async def is_circulating(self):
+        """checks if the tutor's reaction message is still awaiting a response.
+
+        :return: True if the tutor's reaction message is still circulating the queue, otherwise return False.
+        """
+        return self.reaction_msg is None
 
     def mention(self):
         """generate a discord syntax that represents a mention for given Worker.
