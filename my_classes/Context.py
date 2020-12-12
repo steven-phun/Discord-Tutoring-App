b3bf18c9481bdc3fc249a1eaa63f53d0a3e0b3e0
Steven Phun <phun.steven@gmail.com>
2020-12-12 00:28:40 -0800

    fixed when users DM the bot for voice()

--- my_classes/Context.py
+++ my_classes/Context.py
@@ -1,10 +1,30 @@
+import os
+
+
 class Context:
     def __init__(self, ctx):
         self.ctx = ctx  # the discord Context for a given member.
 
-    def voice(self):
-        """represents the member's voice channel
+    def member(self):
+        """:return: class discord.member.Member"""
+        return self.ctx.bot.get_guild(int(os.getenv("GUILD_SERVER_ID"))).get_member(self.ctx.author.id)
 
-        :return: the member's discord.member.VoiceState.
-        """
-        return self.ctx.author.voice
+    def voice(self):
+        """ represents the member's voice state.
+
+        example of instances in voice state:
+            VoiceState:
+                self_mute=bool
+                self_deaf=bool
+                self_stream=bool
+                channel=
+                    VoiceChannel:
+                        id=int
+                        name=str
+                        position=int
+                        bitrate=int
+                        user_limit=int
+                        category_id=int
+
+        :return: the member's discord.member.VoiceState."""
+        return self.member().voice
