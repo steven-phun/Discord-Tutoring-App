874962e5b4c248e774aceec28811648b5040f59c
Steven Phun <phun.steven@gmail.com>
2020-12-12 01:31:01 -0800

    added ctx as one of the attributes

--- my_classes/Worker.py
+++ my_classes/Worker.py
@@ -1,5 +1,9 @@
+from my_classes.Context import Context
+
+
 class Worker:
-    def __init__(self, discord_id, course):
+    def __init__(self, ctx, discord_id, course):
+        self.ctx = Context(ctx)  # the object that represents this member's Content.
         self.discord_id = discord_id  # the tutor's discord id.
         self.course = course  # the course object the tutor is tutoring.
         self.reaction_msg = None  # the reaction message sent by the tutor to get the next student.
@@ -10,13 +14,6 @@ class Worker:
         :return: True if the tutor's reaction message is still circulating the queue, otherwise return False.
         """
         return self.reaction_msg is None
-
-    def mention(self):
-        """generate a discord syntax that represents a mention for given Worker.
-
-        :return: a str that that represents a mention.
-        """
-        return f'<@!{self.discord_id}>'
 
     def hours(self):
         """convert the tutor's current session hours to a string.
