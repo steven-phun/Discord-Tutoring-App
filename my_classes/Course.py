99285685cbfdd0b451b325090ac277b7b513e8c9
Steven Phun <phun.steven@gmail.com>
2020-12-29 16:29:20 -0800

    added hours()

--- my_classes/Course.py
+++ my_classes/Course.py
@@ -1,54 +1,15 @@
-import discord
 import json
 from datetime import datetime
+from my_classes.Schedule import Schedule
 
 
 class Course:
     def __init__(self, code: str = None):
-        self.code = code  # represents the course code.
+        self.schedule = Schedule(code)  # the schedule that corresponds to this course.
+        self.code = code  # str that represents the course code.
         self.message = None  # stores the message sent in the bot announcement channel.
         self.queue = []  # array of student objects that represents the tutoring queue.
         self.size = 0  # the number of students in the queue.
-
-    def hours(self):
-        """get the location, tutoring hours (12 hours format), and tutor's name for given course.
-
-        converts tutoring hours from a .json file to a str to be printed.
-        tutoring hours are stored in a local .json file
-            in a 24 hour time format.
-            for others to modify the hours without touching the code.
-
-        Parameters
-        ----------
-        :return: a str representation of the tutoring hours of a given course.
-        """
-        with open(f'json_files/tutoring_hours/{self.code}.json', encoding="utf8") as file:
-            hours = json.load(file)
-
-        # store schedule in a string.
-        schedule = ''
-        for day in hours:
-            schedule += f'\n__**{day}**__\n'
-
-            # get tutor's schedule.
-            for tutor in hours[day]:
-                # get fields from dictionary.
-                location = hours[day][tutor]['location']
-                start_hour = hours[day][tutor]['start_hour']
-                start_minute = hours[day][tutor]['start_minute']
-                end_hour = hours[day][tutor]['end_hour']
-                end_minute = hours[day][tutor]['end_minute']
-
-                # convert fields to time object.
-                start_time = datetime.now().replace(hour=start_hour, minute=start_minute)
-                end_time = datetime.now().replace(hour=end_hour, minute=end_minute)
-
-                # add tutor's time, location, and tutor's name time to array.
-                tutor_start_time = f'{start_time.strftime("%I:%M%p").lstrip("0").lower()}'
-                tutor_end_time = f'{end_time.strftime("%I:%M%p").lstrip("0").lower()}'
-                schedule += f'**{tutor_start_time} - {tutor_end_time}** [{location}] - *{tutor}*\n'
-
-        return schedule
 
     def update_que(self):
         """determine if the first student has been helped or need help.
